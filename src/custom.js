daeb7e7e5ffa782e4da9ccf20bd2f00b8ed887ac
Gregory Way <gregoryway@comorysystems.com>
2019-12-27 16:17:22 +0000

    first run

---
+++ src/custom.js
@@ -0,0 +1,15 @@
+
+
+
+function myfunction (){
+  alert("test");
+  return "<option>test<option>";
+}
+
+// export for others scripts to use
+//window.$ = $;
+//window.jQuery = jQuery;
+//<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
+
+//$('#myTable').DataTable();    //here is where i call datatable
+  myfunction();
